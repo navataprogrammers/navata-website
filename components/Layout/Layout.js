3723a5fba65d3aacc970bdabafe8dd1129b8711b
prathyusha1215 <prathyusha.galla99@gmail.com>
2025-10-07 15:49:37 +0530

    images

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { useRouter } from 'next/router'; // Import the useRouter hook
+import { useRouter } from 'next/router'; 
 import Navbar from './Navbar';
 import Footer from './Footer'; 
-import ClientBrands from '../ClientBrands';
+import ClientBrands from './ClientBrands';
 
 const Layout = ({ children }) => {
   const router = useRouter();
-  // Check if the current path is the homepage ('/')
+  // homepage ('/')
   const isHomepage = router.pathname === '/';
 
-  // Conditionally create the className string
+  // Conditionally className string
   const mainClassName = isHomepage 
-    ? "main-content homepage-main" // Add a special class for the homepage
-    : "main-content";             // Use the default class for all other pages
+    ? "main-content homepage-main" // class for the homepage
+    : "main-content";             // Default class for all other pages
 
   return (
     <div className="app-container">
       <Navbar />
-      {/* Use the dynamically generated className */}
+      {/*  Dynamically generated className */}
       <main className={mainClassName}>
         {children}
         <ClientBrands />
