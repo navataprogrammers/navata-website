af5b05c281e8f4c69cd07e74d4b9a1bf2b4de9e9
prathyusha1215 <prathyusha.galla99@gmail.com>
2025-10-13 11:49:19 +0530

    adding env variables in nextjs config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    EMAIL_HOST: process.env.EMAIL_HOST,
+    EMAIL_PORT: process.env.EMAIL_PORT,
+    ADMIN_USER: process.env.ADMIN_USER,
+    ADMIN_PASS: process.env.ADMIN_PASS,
+    EMAIL_USER: process.env.EMAIL_USER,
+  },
   reactStrictMode: true,
   images: {
     domains: ['img.icons8.com', 'images.unsplash.com'],
