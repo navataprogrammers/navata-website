e09afddfefbc88823f21000cbd1c71068d3d1ae5
Prathyusha <navataedp@gmail.com>
2025-12-24 10:19:14 +0530

    Update nextjs for blog testing

--- next.config.js
+++ next.config.js
@@ -1,50 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    EMAIL_HOST: process.env.EMAIL_HOST,
-    EMAIL_PORT: process.env.EMAIL_PORT,
-    ADMIN_USER: process.env.ADMIN_USER,
-    ADMIN_PASS: process.env.ADMIN_PASS,
-    EMAIL_USER: process.env.EMAIL_USER,  },
   reactStrictMode: true,
+
   images: {
-    domains: ['img.icons8.com', 'images.unsplash.com', 'jsonplaceholder.typicode.com'], 
+    domains: [
+      'img.icons8.com',
+      'images.unsplash.com',
+      'jsonplaceholder.typicode.com'
+    ],
   },
   async rewrites() {
   return [
-                                                                 
-/*{
-  // Test: Proxy the root GitHub API endpoint- testing purpose
-  source: '/blog',
-  destination: 'https://api.github.com/',                         
-},
-{
-  source: '/blog/:path*',
-  destination: 'https://api.github.com/:path*',
-},*/
-     // 1. Rewrite for all blog paths
+    {
+      source: '/blog/wp-content/:path*',
+      destination: 'https://blog.navata.com/wp-content/:path*',
+    },
+    {
+      source: '/blog/wp-includes/:path*',
+      destination: 'https://blog.navata.com/wp-includes/:path*',
+    },
     {
       source: '/blog/:path*',
       destination: 'https://blog.navata.com/:path*',
     },
-
-    // 2. Rewrite for root /blog page
-    {   
+    {
       source: '/blog',
       destination: 'https://blog.navata.com',
     },
-
-    // 3. WordPress assets
-    {
-      source: '/wp-content/:path*',
-      destination: 'https://blog.navata.com/wp-content/:path*',
-    },
-    {
-      source: '/wp-includes/:path*',
-      destination: 'https://blog.navata.com/wp-includes/:path*',
-    },
   ];
+  }
 }
-};
 
 module.exports = nextConfig;
